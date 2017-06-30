26ece9cd5442f3f0caf1ce5d664d2530a2625f50
turbaevsky <turbaevsky@gmail.com>
2017-06-30 15:10:43 +0100

    Add files via upload

--- m.r
+++ m.r
@@ -1,13 +1,21 @@
 library(RCurl)
 library(jsonlite)
 library(plyr)
+library(quantmod)
 
 
 dT <- function(d) {return(as.Date(d,'%d-%b-%y'))}
+longDate <- function(d) {return(as.Date(as.character(d),'%A, %B %d, %Y'))}
+
+#' Data convertor from finantial time
+toXTS <- function(data){
+    d <- cbind(as.Date(apply(data[1],2,longDate)),data[2:5])
+    return(xts(d[,-1], order.by=d[,1]))
+    }
 
 ##################### Plot last ####################
-plotLast <- function(){
-x <- getURL("http://www.google.com/finance/historical?q=NASDAQ:GOOG&startdate=Jan+01%2C+2017&enddate=Jun+26%2C+2017&output=csv")
+plotLast <- function(name,start='Jan+01%2C+2017',end='Jun+26%2C+2017'){
+x <- getURL(paste("http://www.google.com/finance/historical?q=NASDAQ:",name,"&startdate=",start,"&enddate=",end,"&output=csv",sep=''))
 y <- read.csv(text = x)
 y <- cbind(as.Date(apply(y[1],2,dT)),y[2:5])
 plot(y[[1]],y[[2]],'l')
@@ -17,5 +25,8 @@ plot(y[[1]],y[[2]],'l')
 source('~/Desktop/flipsideR-master/R/option-chain.R')
 source('~/Desktop/flipsideR-master/R/configure.R')
 ########################################################
-AAPL = getOptionChain("GOOG")
-print(head(AAPL))
+ck92 <- read.csv('~/Desktop/b886ck92.csv')
+ck <- toXTS(ck92)
+chartSeries(ck)
+addSMA()
+weeklyReturns(ck)
