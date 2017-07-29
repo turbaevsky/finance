d16e3ec58b815a219b0fa19d9abe569db2793020
turbaevsky <turbaevsky@gmail.com>
2017-07-29 13:28:03 +0100

    Add files via upload

---
+++ m2.r
@@ -0,0 +1,11 @@
+library(quantmod)
+
+lst <- c('WIZZ.L','KAZ.L','VED.L','ICP.L','RIO.L','STAN.L','JUP.L')
+
+for (n in lst) {
+    obj <- getSymbols(n,src='google',env=NULL)
+    chartSeries(obj,subset='last 4 months',TA=c(addSMA(),addEMA(30),addMACD(),addSMA(200),addVo()),multi.col=FALSE,name=n)
+    invisible(readline(prompt="Press [enter] to continue"))
+    chartSeries(obj,subset='last 9 months',TA=c(addSMA(),addEMA(30),addMACD(),addSMA(200),addVo()),multi.col=FALSE,name=n)
+    invisible(readline(prompt="Press [enter] to continue"))
+    }
