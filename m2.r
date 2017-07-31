02fcbf6175c25decdc0f6736168e71a2a5830bd7
turbaevsky <turbaevsky@gmail.com>
2017-07-31 14:10:43 +0100

    Add files via upload

--- m2.r
+++ m2.r
@@ -4,6 +4,18 @@ lst <- c('WIZZ.L','KAZ.L','VED.L','ICP.L','RIO.L','STAN.L','JUP.L','IHG.L')
 
 for (n in lst) {
     obj <- getSymbols(n,src='google',env=NULL)
+    # Update quotes
+    q <- getQuote(n)
+    d <- Sys.Date()
+    row.names(q)<- trunc(q[,"Trade Time"], units="days")
+    q <- q[,c("Open","High","Low","Last","Volume")]
+    names(q) <- c("Open","High","Low","Close","Volume")
+    q <- xts(q,d)
+    #print(q)
+                                        #obj <- merge(obj,q,by="Date")
+    obj <- rbind(obj,q)
+    print(tail(obj))
+    # Chart
     chartSeries(obj,subset='last 4 months',TA=c(addSMA(),addEMA(30),addMACD(),addSMA(200),addVo()),multi.col=FALSE,name=n)
     invisible(readline(prompt="Press [enter] to continue"))
     chartSeries(obj,subset='last 9 months',TA=c(addSMA(),addEMA(30),addMACD(),addSMA(200),addVo()),multi.col=FALSE,name=n)
