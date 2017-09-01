35b6f35f6e9b16d0f75b3949c5dd130fd04fdfa6
turbaevsky <turbaevsky@gmail.com>
2017-09-01 16:47:35 +0100

    Add files via upload

--- m2.r
+++ m2.r
@@ -1,4 +1,6 @@
 library(quantmod)
+library(RCurl)
+library(jsonlite)
 
 sp500 <- c('MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET','AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE','AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANDV','ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','ADM','ARNC','AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BHGE','BLL','BAC','BK','BCR','BAX','BBT','BDX','BRK.B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX','BHF','BMY','AVGO','BF.B','CHRW','CA','COG','CPB','COF','CAH','CBOE','KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW','CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CLX','CME','CMS','COH','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP','ED','STZ','COO','GLW','COST','COTY','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH','DG','DLTR','D','DOV','DOW','DPS','DTE','DRE','DD','DUK','DXC','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','RE','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST','FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F','FTV','FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GGP','GIS','GM','GPC','GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HRS','HIG','HAS','HCA','HCP','HP','HSIC','HSY','HES','HPE','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','IDXX','INFO','ITW','ILMN','IR','INTC','ICE','IBM','INCY','IP','IPG','IFF','INTU','ISRG','IVZ','IRM','JEC','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LEG','LEN','LVLT','LUK','LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','KORS','MCHP','MU','MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS','NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','PCAR','PKG','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RJF','RTN','O','RHT','REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV','SPGI','SWK','SPLS','SBUX','STT','SRCL','SYK','STI','SYMC','SYF','SNPS','SYY','TROW','TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','TRV','TRIP','FOXA','FOX','TSN','UDR','ULTA','USB','UA','UAA','UNP','UAL','UNH','UPS','URI','UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','HCN','WDC','WU','WRK','WY','WHR','WFM','WMB','WLTW','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS')
 
@@ -8,10 +10,10 @@ ftse250 <- c('3IN.L','AA.L','ASL.L','ACA.L','AGK.L','ALD.L','ATST.L','AMFW.L','A
 #####################################################################
 shortlst <- c('FXPO.L','KAZ.L','WIZZ.L')
 excluded <- c('GNC.L','IWG.L')
-lim <- c(286.1,0,0,0,0) # Stop-loss limit
-stocks <- c(350,0,0,0,0,0) # No of stocks
-tax <- c((10.5+4.96),0,0,0,0,0) # Taxes
-long <- c(280.44,0,0,0,0,0) # Buying price
+lim <- c(0,0,0,0,0) # Stop-loss limit
+stocks <- c(0,0,0,0,0,0) # No of stocks
+tax <- c(0,0,0,0,0,0) # Taxes
+long <- c(0,0,0,0,0,0) # Buying price
 #lst <- shortlst
 #quotes <- TRUE
 #web <- TRUE
@@ -20,6 +22,12 @@ long <- c(280.44,0,0,0,0,0) # Buying price
 #####################################################################
 ### Functions ###
 #####################################################################
+gQuote <- function(n='FXPO.L'){ # get quotes from google (real-time)
+    url <- paste('http://finance.google.com/finance/info?client=ig&q=',n,sep='')
+    data <- getURL(url)
+    json <- fromJSON(gsub('\n|[//]|[\\]','',data))
+    return(paste(json$lt,json$l))
+}
 updQuote <- function(obj,n) #Update price table by actual quotes
 {
     q <- getQuote(n)
@@ -128,7 +136,7 @@ analyse <- function(lst=ftse250,quotes=TRUE,web=FALSE,change=5,adx=25){
                 invisible(readline(prompt="Press [enter] to continue"))
             }})}}}
 
-ticker <- function(n='FXPO.L',min=10){
+ticker <- function(n='FXPO.L',min=10,subset='last 3 months'){
     obj <- getSymbols(n,src='google',env=NULL)
     obj <- na.omit(obj)
     while (T) {
@@ -139,9 +147,10 @@ ticker <- function(n='FXPO.L',min=10){
             cls <- getQuote(n)[[2]]
             tm <- getQuote(n)[[1]]
             balance <- stocks[no]/100*cls-tax[no]-stocks[no]/100*long[no]
-            print(paste(format(Sys.time(),'%H:%M'),'Balance=',signif(balance,3),'Last=',cls,'(',tm,')'))
+            #print(paste(format(Sys.time(),'%H:%M'),'Balance=',signif(balance,3),'Last=',cls,'(',tm,')'))
+            print(gQuote(n))
         }
-        chartSeries(obj,subset='last 2 months',TA=c(addSMA(),addEMA(30),addBBands(),addMACD(),addVo()),multi.col=FALSE,name=n,log.scale=T)
+        chartSeries(obj,subset=subset,TA=c(addSMA(),addEMA(30),addBBands(),addMACD(),addVo()),multi.col=FALSE,name=n,log.scale=T)
         plot(addLines(h=lim[no],col='red'))
         Sys.sleep(60*min)}
 }
